0afac292ea92c84c8d905465ce15b3719896624a
YouKnowAsura3 <mkrishnababu116@gmail.com>
2025-05-08 08:19:30 +0530

    Update portscanner.py

--- portscanner.py
+++ portscanner.py
@@ -9,6 +9,11 @@ OUTPUT_FILE = 'live_rdp_verified.txt'
 BOT_TOKEN = '7390307264:AAH9pZEC2i6xrOe67eOQi2i-4r3cIZVaA-k'
 CHAT_ID = '5326706151'
 
+lock = threading.Lock()
+total_ips = 0
+scanned = 0
+good = 0
+
 def is_windows_rdp(ip):
     try:
         result = subprocess.run(
@@ -21,35 +26,53 @@ def is_windows_rdp(ip):
         return False
 
 def check_and_save(ip):
-    print(f"[SCAN] {ip} - checking if Windows RDP...")
+    global scanned, good
+    subprocess.run(['echo', f'[SCAN] {ip} - checking if Windows RDP...'])
     if is_windows_rdp(ip):
-        print(f"[WIN-RDP] {ip}")
-        with open(OUTPUT_FILE, 'a+') as f:
-            f.seek(0)
-            if ip not in f.read():
-                f.write(ip + '\n')
+        subprocess.run(['echo', f'[WIN-RDP] {ip}'])
+        with lock:
+            with open(OUTPUT_FILE, 'a+') as f:
+                f.seek(0)
+                if ip not in f.read():
+                    f.write(ip + '\n')
+                    good += 1
     else:
-        print(f"[NOT Windows] {ip}")
+        subprocess.run(['echo', f'[NOT Windows] {ip}'])
+    with lock:
+        scanned += 1
+
+def status_updater():
+    while True:
+        time.sleep(120)
+        with lock:
+            bad = scanned - good
+            subprocess.run(['echo', f'SCANNED : {scanned}'])
+            subprocess.run(['echo', f'GOOD    : {good}'])
+            subprocess.run(['echo', f'BAD     : {bad}'])
+            subprocess.run(['echo', f'TOTAL   : {total_ips}'])
 
 def send_file_to_telegram():
-    print("[!] 3h15m reached. Sending file to Telegram...")
+    subprocess.run(['echo', '[!] 3h15m reached. Sending file to Telegram...'])
     url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendDocument"
     try:
         with open(OUTPUT_FILE, 'rb') as file:
             requests.post(url, data={'chat_id': CHAT_ID}, files={'document': file})
-        print("[+] File sent to Telegram.")
+        subprocess.run(['echo', '[+] File sent to Telegram.'])
     except Exception as e:
-        print(f"[!] Failed to send file: {e}")
+        subprocess.run(['echo', f'[!] Failed to send file: {e}'])
 
 def schedule_file_send():
     time.sleep(3 * 3600 + 15 * 60)
     send_file_to_telegram()
 
 def main():
+    global total_ips
     threading.Thread(target=schedule_file_send, daemon=True).start()
+    threading.Thread(target=status_updater, daemon=True).start()
 
     with open(INPUT_FILE, 'r') as f:
         ips = [line.strip() for line in f if line.strip()]
+        total_ips = len(ips)
 
     with ThreadPoolExecutor(max_workers=100) as executor:
         executor.map(check_and_save, ips)
